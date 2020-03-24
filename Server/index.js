332f99c6899e80e83fcf5cd469a4f3790767c3b7
Matthew Kanderka <kanderkam@hotmail.com>
2020-03-23 19:28:51 -0600

    socket setup

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
+
+// Socket.io
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
+
+var usersMap = new Map();
+var rooms = ['Lobby'];
+
 // Middleware
 app.use(bodyParser.json())
 app.use(cors())
@@ -12,5 +24,42 @@ app.use(cors())
 const users = require('./userCollection')
 app.use('/api/users',users)
 
+// Socket setup
+io.on('connection', function(socket) {
+
+    console.log("user joined");
+    socket.on('newUser', function(username){
+        socket.username = username;
+        usersMap.set(socket.id, username);
+        socket.room = 'Lobby';
+        socket.join('Lobby');
+    });
+
+    socket.on('createRoom', function(room) {
+        rooms.push(room);
+    });
+
+    socket.on('pieceMoved', function(){
+
+    });
+
+    socket.on('changeRoom', function(newRoom) {
+        var oldRoom = socket.room;
+        socket.leave(socket.room);
+        socket.join(newRoom);
+        socket.room = newRoom;
+    });
+
+    socket.on('disconnect', function() {
+        usersMap.delete(socket.id);
+        socket.leave(socket.room);
+        console.log("user left");
+    });
+});
+
 const port = process.env.PORT || 9000
 app.listen(port, ()=>console.log(`Server started on port: ${port}`))
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
