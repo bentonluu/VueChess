deb18a273f63a4391e6c398b13361065e76d6b66
Matthew Kanderka <kanderkam@hotmail.com>
2020-04-13 21:11:50 -0600

    fixed delete message still active 2

--- VueChess/src/components/TournamentDetails.vue
+++ VueChess/src/components/TournamentDetails.vue
@@ -60,6 +60,7 @@ export default {
     methods: {
         close() {
             this.tournamentDeleted = false
+            this.deleteDisable = false
             this.$emit('close');
         },
         editTournament() {
